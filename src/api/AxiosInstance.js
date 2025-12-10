3848ee8d52f7f12bc9e0f4e114850a799c0928d3
Aswana <aswanaanu201@gmail.com>
2025-12-11 00:12:35 +0530

    commit

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE || "https://mainproject-8vc6.onrender.com",
   withCredentials: false, // using Authorization header, not cookies
 });
 
