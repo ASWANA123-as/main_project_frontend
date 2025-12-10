a2bc5795dd24d3594fc6d0c93a2ab0ae0fd289a4
Aswana <aswanaanu201@gmail.com>
2025-12-10 20:52:01 +0530

    commit

--- src/Pages/Attandee/MyTickets.jsx
+++ src/Pages/Attandee/MyTickets.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getMyEvents,downloadTicket } from "../../api/attandee"; 
-import { toast } from "react-toastify";
+
 import { Link } from "react-router-dom";
 
 const handleDownload = async (userId,eventId, title) => {
