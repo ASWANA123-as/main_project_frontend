cb60e24201387e1c58f347df6c92bf011d17f71b
Aswana <aswanaanu201@gmail.com>
2025-12-12 23:43:30 +0530

    ttttt

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,22 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 dark:text-white transition">
-      {/* <Header/> */}
-<ThemeToggle/>
-      <main className="flex-grow bg-gray-50 px-6 py-8">
+    <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 dark:text-white transition-colors duration-300">
+
+      {/* Header (optional) */}
+      {/* <Header /> */}
+
+      {/* Theme Toggle Positioned Top-Right */}
+      <div className="w-full flex justify-end px-6 py-4">
+        <ThemeToggle />
+      </div>
+
+      {/* Main Content */}
+      <main className="flex-grow px-6 py-8 bg-gray-50 dark:bg-gray-800 rounded-lg">
         {children}
       </main>
 
+      {/* Footer */}
       <Footer />
     </div>
   );
