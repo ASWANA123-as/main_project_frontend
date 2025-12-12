f87835819f5a3de18daee6094036ddc28834023a
Aswana <aswanaanu201@gmail.com>
2025-12-12 22:34:48 +0530

    eeeeeeeeeeeee

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App";
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+ <Provider store={store}>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </Provider>
 );
